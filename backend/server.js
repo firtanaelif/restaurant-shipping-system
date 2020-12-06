564c39d1145bc35d4806197ab65126740f914cf4
firtanaelif <firtana.elif@gmail.com>
2020-12-07 01:39:16 +0300

    Connected error mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import data from './data';
-import fs from 'fs';
+import config from './config';
+import userRouter from './routers/userRouter';
+
+mongoose
+    .connect(config.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log('Connected to mongodb.');
+    })
+    .catch((error) => {
+        console.log('Connected not mongodb.',error.reason);
+    });
 
 const app = express();
 app.use(cors());
+app.use('/api/users', userRouter);
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
